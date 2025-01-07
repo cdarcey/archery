4caa52e96fe1d0946fc60a9ed00adc29d27fe429
Carey Darcey <careydarcey@rocketmail.com>
2025-01-07 13:20:48 -0600

    new Repo

---
+++ build.bat
@@ -0,0 +1,60 @@
+@rem keep environment variables modifications local
+@setlocal
+
+@rem make script directory CWD
+@pushd %~dp0
+
+@rem modify PATH to find vcvarsall.bat
+@set PATH=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build;%PATH%
+@set PATH=C:\Program Files\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build;%PATH%
+@set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build;%PATH%
+@set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build;%PATH%
+@set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise/VC\Auxiliary\Build;%PATH%
+
+@rem setup environment for MSVC dev tools
+@call vcvarsall.bat amd64 > nul
+
+@rem default compilation result
+@set PL_RESULT=[1m[92mSuccessful.[0m
+
+@rem create main target output directoy
+@if not exist "./out" @mkdir "./out"
+
+@rem cleanup binaries if not hot reloading
+@if exist "./out/game.exe" del ".\out\game.exe"
+@if exist "./out/cpptest_*.pdb" del ".\out\cpptest_*.pdb"
+
+@rem create output directory
+@if not exist "./out" @mkdir "./out"
+
+@rem run compiler (and linker)
+@echo.
+@echo [1m[93m~~~~~~~~~~~~~~~~~~~~~~[0m
+@echo [1m[36mCompiling and Linking...[0m
+
+@rem call compiler
+cl game.c -Fe"./out/game.exe" -Fo"./out/" -Od -Zi -nologo -link -incremental:no 
+
+@rem check build status
+@set PL_BUILD_STATUS=%ERRORLEVEL%
+
+@rem failed
+@if %PL_BUILD_STATUS% NEQ 0 (
+    @echo [1m[91mCompilation Failed with error code[0m: %PL_BUILD_STATUS%
+    @set PL_RESULT=[1m[91mFailed.[0m
+    goto Cleanupcpptest
+)
+
+@rem cleanup obj files
+:Cleanupcpptest
+    @echo [1m[36mCleaning...[0m
+    @REM @del ".\out\*.obj"  > nul 2> nul
+
+
+@rem print results
+@echo.
+@echo [36mResult: [0m %PL_RESULT%
+@echo [36m~~~~~~~~~~~~~~~~~~~~~~[0m
+
+@rem return CWD to previous CWD
+@popd
