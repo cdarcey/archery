8a893cbbb66e7e9191e02a1d393c1a19b9b02d84
Carey Darcey <careydarcey@rocketmail.com>
2025-01-18 07:30:13 -0600

    rasterize multiple triangles

--- rasterize.c
+++ rasterize.c
@@ -59,29 +59,29 @@ int main()
     };
 
     cd_Vertex a = {
-        .xPos = 50,
-        .yPos = 200,
+        .xPos = 10,
+        .yPos = 100,
         .r    = 255,
         .g    = 0,
         .b    = 0
     };
 
     cd_Vertex b = {
-        .xPos = 125,
-        .yPos = 50,
+        .xPos = 75,
+        .yPos = 10,
         .r    = 0,
         .g    = 255,
         .b    = 0
     };
     cd_Vertex c = {
-        .xPos = 200,
-        .yPos = 200,
+        .xPos = 100,
+        .yPos = 100,
         .r    = 0,
         .g    = 0,
         .b    = 255
     };
     cd_Vertex d = {
-        .xPos = 25,
+        .xPos = 125,
         .yPos = 175,
         .r    = 255,
         .g    = 0,
@@ -89,23 +89,23 @@ int main()
     };
 
     cd_Vertex e = {
-        .xPos = 100,
-        .yPos = 25,
+        .xPos = 200,
+        .yPos = 125,
         .r    = 0,
         .g    = 255,
         .b    = 0
     };
     cd_Vertex f = {
-        .xPos = 175,
-        .yPos = 175,
+        .xPos = 200,
+        .yPos = 200,
         .r    = 0,
         .g    = 0,
         .b    = 255
     };
 
-    cd_Vertex atVertexBuffer[3] = {a, b, c};
+    cd_Vertex atVertexBuffer[6] = {a, b, c, d, e, f};
 
-    cd_rasterize_triangles(&tData, atVertexBuffer, 3);
+    cd_rasterize_triangles(&tData, atVertexBuffer, 6);
     cd_output_frame_buffer(&tData);
     
 }
@@ -219,9 +219,9 @@ void cd_rasterize_triangle(cd_Data* ptData, cd_Vertex a, cd_Vertex b, cd_Vertex 
             {
                 unsigned char r = (unsigned char)((float)rColor.r * weightA + (float)gColor.r * weightB + (float)bColor.r * weightC);
                 unsigned char g = (unsigned char)((float)rColor.g * weightA + (float)gColor.g * weightB + (float)bColor.g * weightC);
-                unsigned char b0 = (unsigned char)((float)rColor.b * weightA + (float)gColor.b * weightB + (float)bColor.b * weightC);
+                unsigned char b = (unsigned char)((float)rColor.b * weightA + (float)gColor.b * weightB + (float)bColor.b * weightC);
                 
-                Color colorP = {r, g, b0};
+                Color colorP = {r, g, b};
                 cd_set_pixel(ptData, p, colorP);
             }
         }
@@ -238,24 +238,24 @@ void cd_rasterize_triangles(cd_Data* ptData, cd_Vertex* atVerticies, int iVertex
     };
 
     /* edge function for entire triangle */
-    float ABC = (float)(atVerticies[1].xPos - atVerticies[0].xPos) * (atVerticies[2].yPos - atVerticies[0].yPos) - (atVerticies[1].yPos - atVerticies[0].yPos) * (atVerticies[2].xPos - atVerticies[1].xPos);
+    float ABC = (float)cd_edgeFunction(atVerticies[0], atVerticies[1], atVerticies[2]);
 
     /* min & max to only check pixels in a bounding box*/
-    const int minX = cd_minNum(atVerticies[0].xPos, atVerticies[1].xPos, atVerticies[2].xPos);
-    const int minY = cd_minNum(atVerticies[0].yPos, atVerticies[1].yPos, atVerticies[2].yPos);
-    const int maxX = cd_maxNum(atVerticies[0].xPos, atVerticies[1].xPos, atVerticies[2].xPos);
-    const int maxY = cd_maxNum(atVerticies[0].yPos, atVerticies[1].yPos, atVerticies[2].yPos);  
+    // const int minX = cd_minNum(atVerticies[0].xPos, atVerticies[1].xPos, atVerticies[2].xPos);
+    // const int minY = cd_minNum(atVerticies[0].yPos, atVerticies[1].yPos, atVerticies[2].yPos);
+    // const int maxX = cd_maxNum(atVerticies[0].xPos, atVerticies[1].xPos, atVerticies[2].xPos);
+    // const int maxY = cd_maxNum(atVerticies[0].yPos, atVerticies[1].yPos, atVerticies[2].yPos);  
 
     /* loop and set pixels inside triangle */
-    // for(int i = 0; i < iVertexCount; i++)
-    // {
+    for(int i = 0; i < iVertexCount; i += 3)
+    {
         for(cd_P.yPos = 0; cd_P.yPos < 256; cd_P.yPos++)
         {
             for(cd_P.xPos = 0; cd_P.xPos < 256; cd_P.xPos++)
             {
-                const int ABP = cd_edgeFunction(atVerticies[0], atVerticies[1], cd_P);
-                const int BCP = cd_edgeFunction(atVerticies[1], atVerticies[2], cd_P);
-                const int CAP = cd_edgeFunction(atVerticies[2], atVerticies[0], cd_P);
+                const int ABP = cd_edgeFunction(atVerticies[i], atVerticies[i + 1], cd_P);
+                const int BCP = cd_edgeFunction(atVerticies[i + 1], atVerticies[i + 2], cd_P);
+                const int CAP = cd_edgeFunction(atVerticies[i + 2], atVerticies[i], cd_P);
 
                 const float weightA = BCP / ABC;
                 const float weightB = CAP / ABC;
@@ -263,16 +263,16 @@ void cd_rasterize_triangles(cd_Data* ptData, cd_Vertex* atVerticies, int iVertex
 
                 if(ABP >= 0 && BCP >= 0 && CAP >= 0)
                 {
-                    unsigned char red   = (unsigned char)((float)atVerticies[0].r * weightA + (float)atVerticies[1].r * weightB + (float)atVerticies[2].r * weightC);
-                    unsigned char green = (unsigned char)((float)atVerticies[0].g * weightA + (float)atVerticies[1].g * weightB + (float)atVerticies[2].g * weightC);
-                    unsigned char blue  = (unsigned char)((float)atVerticies[0].b * weightA + (float)atVerticies[1].b * weightB + (float)atVerticies[2].b * weightC);
+                    unsigned char red   = (unsigned char)((float)(atVerticies[0].r * weightA) + (float)(atVerticies[1].r * weightB) + (float)(atVerticies[2].r * weightC));
+                    unsigned char green = (unsigned char)((float)(atVerticies[0].g * weightA) + (float)(atVerticies[1].g * weightB) + (float)(atVerticies[2].g * weightC));
+                    unsigned char blue  = (unsigned char)((float)(atVerticies[0].b * weightA) + (float)(atVerticies[1].b * weightB) + (float)(atVerticies[2].b * weightC));
 
                     cd_Color colorP = {red, green, blue};
                     cd_set_pixel(ptData, cd_P, colorP);
                 }
             }
         }
-    // }
+    }
 }
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
